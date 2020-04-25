bb118819e057feaafa1184d8af560f19ea08dc07
ginowine <ginowine1@gmail.com>
2020-04-25 13:31:33 +0100

    scheme fixed

--- src/main/java/com/example/DogGraphQL/DogGraphQlApplication.java
+++ src/main/java/com/example/DogGraphQL/DogGraphQlApplication.java
@@ -1,13 +1,61 @@
 package com.example.DogGraphQL;
 
+import com.example.DogGraphQL.exception.GraphQLErrorAdapter;
+import com.example.DogGraphQL.mutator.Mutation;
+import com.example.DogGraphQL.repository.DogRepository;
+import com.example.DogGraphQL.resolver.Query;
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.servlet.GraphQLErrorHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DogGraphQlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DogGraphQlApplication.class, args);
+	}
+
+	@Bean
+	public GraphQLErrorHandler errorHandler() {
+		return new GraphQLErrorHandler() {
+			@Override
+			public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+				List<GraphQLError> clientErrors = errors.stream()
+						.filter(this::isClientError)
+						.collect(Collectors.toList());
+
+				List<GraphQLError> serverErrors = errors.stream()
+						.filter(e -> !isClientError(e))
+						.map(GraphQLErrorAdapter::new)
+						.collect(Collectors.toList());
+
+				List<GraphQLError> e = new ArrayList<>();
+				e.addAll(clientErrors);
+				e.addAll(serverErrors);
+				return e;
+			}
+
+			protected boolean isClientError(GraphQLError error) {
+				return !(error instanceof ExceptionWhileDataFetching || error instanceof Throwable);
+			}
+		};
+	}
+
+	@Bean
+	public Query query(DogRepository dogRepository) {
+		return new Query(dogRepository);
+	}
+
+	@Bean
+	public Mutation mutation(DogRepository dogRepository) {
+		return new Mutation(dogRepository);
 	}
 
 }
